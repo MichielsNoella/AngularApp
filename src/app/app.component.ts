dda60eafc00f04dacb571e93cb82411631b74a21
Noella Michiels <michiels.noella@skynet.be>
2019-05-13 19:57:29 +0200

    Eenvoudige databinding {{}} - Simple databinding {{}}

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'noella-app';
+
+// first use
+// export class AppComponent {
+//   title: string = 'First Angular-app';
+//   name: string = 'Noella';
+// }
+// dit kan ook maar is niet zo'n goede gewoonte, dit is niet voor de constructor maar voor de methode ngOnInit()
+// export class AppComponent {
+//   title: string;
+//   name: string;
+//
+//   constructor() {
+//     this.title = 'First Angular-app';
+//     this.name = 'Noella';
+//   }
+// }
+
+// best use
+export class AppComponent implements OnInit {
+  title: string;
+  name: string;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    this.title = 'First Angular-app';
+    this.name = 'Noella';
+  }
 }
